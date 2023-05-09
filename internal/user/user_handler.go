40e2eb2827a9c8fbac332100dcfbbfd234c1cdde
Vickler Charles <charlesvikler@gmail.com>
2023-05-09 23:15:31 +0200

    updated smae site

--- internal/user/user_handler.go
+++ internal/user/user_handler.go
@@ -57,7 +57,7 @@ func (h *Handler) Login(c *gin.Context) {
 		MaxAge:   expiration,
 		HttpOnly: true, 
 		Secure:   true, 
-		SameSite: http.SameSiteLaxMode, 
+		SameSite: http.SameSiteNoneMode, 
 	}
 
 	http.SetCookie(c.Writer, cookie)
